38c8a894281f89c624d8cb1d91f3427ef90a4077
fernandezabel <163901256+fernandezabel@users.noreply.github.com>
2024-04-22 15:26:11 -0300

    Add files via upload

---
+++ tp 2 programacion punto 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	int x,c;
+	i=4;
+	j=i+7;
+	
+	x=9;
+	x=x*4;
+	cout<<i<<","<<j<<","<<c<<","<<x<<"\n";
+	return 0;
+}
---
+++ tp 2 programacion punto 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float sale, tax;
+	float tax_rate=0.008;
+	sale=22.54;
+	tax = sale * tax_rate;
+	
+	cout <<"the tax sale"<<tax<<"\n";
+	return 0;
+}
